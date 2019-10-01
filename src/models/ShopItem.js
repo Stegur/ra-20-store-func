57bfbc903804a963cf4633bd24c5700fb7c3722c
Stegura Alex <stegura.alex@gmail.com>
2019-10-01 17:21:46 +0300

    task

---
+++ src/models/ShopItem.js
@@ -0,0 +1,12 @@
+class ShopItem {
+    constructor(values) {
+        this.brand = values.brand;
+        this.title = values.title;
+        this.description = values.description;
+        this.descriptionFull = values.descriptionFull;
+        this.price = values.price;
+        this.currency = values.currency;
+    }
+}
+
+export default ShopItem;
